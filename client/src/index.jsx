e503de436357a18e65c765d978761c3d75a20574
Sandy Chu <sandy.chu.40@gmail.com>
2023-01-25 10:13:36 -0800

    Testing git changes

---
+++ client/src/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+
+
+
+
+
+  return (
+    <div>
+      This is a placeholder being served
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
