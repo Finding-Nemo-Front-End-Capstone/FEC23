ae2c81bbcc94fed138d82101302ceec0d4989d47
Britta Moore <brittacmoore@gmail.com>
2023-01-26 10:22:01 -0800

    save changes before eslint updates

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -3,6 +3,25 @@ import React from 'react';
 const Overview = ({ product }) => {
   console.log('current product', product)
 
+  useEffect(() => {
+    axios.get('/styles/' + currentId)
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((err) => { console.log('there was an error', err); })
+  }, [])
+
+  // const cards = relatedIds.map((singleId) => {
+  //   axios.get('/styles/' + singleId)
+  //   .then((data) => {
+  //     return (
+  //       <div>
+
+  //       </div>
+  //     )
+  //   })
+  // })
+
   return (
     <div>hi from Overview
       <h1>{product.name}</h1>
