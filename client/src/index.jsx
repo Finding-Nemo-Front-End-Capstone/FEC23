22f8f2e6d33d984bd1417c3b04057c50cec52e55
Sandy Chu <sandy.chu.40@gmail.com>
2023-01-26 09:50:25 -0800

    Commented out code within index.jsx for personal testing

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import RelatedProducts from './components/RelatedProducts.jsx'
 import Questions from './components/Questions.jsx';
-import Overview from './components/Overview.jsx';
+// import Overview from './components/Overview.jsx';
 import Ratings from './components/Ratings.jsx';
 import axios from 'axios'
 
@@ -10,18 +10,18 @@ const App = () => {
 // const [productList, setProductList] = useState([]);
 const [product, setProduct] = useState([]);
 
-useEffect(() => {
-  axios.get('/products')
-    .then((data) => {setProduct(data.data[0])})
-    .catch(() => console.log('err in fetching product List'))
-}, [])
+// useEffect(() => {
+//   axios.get('/products')
+//     .then((data) => {setProduct(data.data[0])})
+//     .catch(() => console.log('err in fetching product List'))
+// }, [])
 
 
 
   return (
     <div>
       This is a placeholder being served
-      <Overview product={product}/>
+      {/* <Overview product={product}/> */}
       <Ratings product={product}/>
       <Questions product={product}/>
       <RelatedProducts product={product} setProduct={setProduct}/>
