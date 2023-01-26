02c24992720d01d0387a718da35e58a0aa63a75a
Britta Moore <brittacmoore@gmail.com>
2023-01-25 16:45:12 -0800

    save changes to Overview before pull main

--- client/src/Overview.jsx
+++ client/src/Overview.jsx
@@ -1,3 +1,13 @@
 import React from 'react';
 
+const Overview = ({ product }) => {
+
+
+  return (
+    <div>hi from Overview
+      <h1>{product.title}</h1>
+    </div>
+  )
+};
+
 export default Overview;
