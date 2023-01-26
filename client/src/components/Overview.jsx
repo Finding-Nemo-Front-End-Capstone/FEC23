ae1af505b5d49bfa06bbb15a2d04f822b07d8042
Nathanael <nathanaeltjen@hotmail.com>
2023-01-26 11:35:35 -0800

    newest 1/26/23 11:35am

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,3 +1,15 @@
 import React from 'react';
 
+function Overview({ product, rating }) {
+  return (
+    <div>
+      hi from Overview
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>{product.default_price}</p>
+      {/* <img src={}/></img> */}
+    </div>
+  );
+}
+
 export default Overview;
