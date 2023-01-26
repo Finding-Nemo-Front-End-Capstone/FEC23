76fcb6c999f7bb2ad8497647cea9d02448ee3369
Britta Moore <brittacmoore@gmail.com>
2023-01-25 16:50:53 -0800

    un-comment import Overview

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Questions from './Questions.jsx';
-// import Overview from './Overview.jsx';
+import Overview from './Overview.jsx';
 import Ratings from './Ratings.jsx';
 import axios from 'axios'
 
