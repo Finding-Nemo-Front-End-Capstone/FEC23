2d3c878bbb8169b3339ccef96ca7385c02ddd9fb
Sandy Chu <sandy.chu.40@gmail.com>
2023-01-26 14:10:54 -0800

    ESLint conformity  (#13)
    * Fix ESLint formatting
    
    * refactored code to follow ESLint

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+/* eslint-disable no-console */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import RelatedCards from './RelatedCards.jsx';
 
 function RelatedProducts({ id }) {
   const [relatedIds, setRelatedIds] = useState([]);
   const [currentId, setCurrentId] = useState(id === undefined ? 40344 : id);
   useEffect(() => {
-    axios.get('/db/related/' + currentId)
-    .then((data) => { setRelatedIds(data.data); })
-    .catch((err) => { console.log('there was an error', err); })
-  }, [])
+    axios.get(`/db/related/${currentId}`)
+      .then((data) => { setRelatedIds(data.data); })
+      .catch((err) => { console.log('there was an error', err); });
+  }, []);
 
-  const cards = relatedIds.map((singleId) => {
-    return (
-      <div className="relatedCard">
-        {singleId}
-        <RelatedCards id={singleId} />
-      </div>
-    )
-  })
+  const cards = relatedIds.map((singleId) => (
+    <div className="relatedCard">
+      {singleId}
+      <RelatedCards id={singleId} />
+    </div>
+  ));
 
   return (
     <div className="relatedContainer">
