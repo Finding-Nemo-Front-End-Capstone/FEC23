765ac10af4f5472a9adde5aa7c34b5646068cd8d
Britta Moore <brittacmoore@gmail.com>
2023-01-26 09:48:53 -0800

    save Overview changes before pull main

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const Overview = ({ product }) => {
-
+  console.log('current product', product)
 
   return (
     <div>hi from Overview
-      <h1>{product.title}</h1>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>{product.default_price}</p>
+      <img src={}/></img>
     </div>
   )
 };
