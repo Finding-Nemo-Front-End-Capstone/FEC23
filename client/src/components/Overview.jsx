b6f63ec0f8891c4d533390d436e77f72c6001a0d
Britta Moore <brittacmoore@gmail.com>
2023-01-26 14:29:45 -0800

    save changes before pull main

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
-const Overview = ({ product }) => {
+const Overview = ({ product, rating }) => {
   console.log('current product', product)
 
+  if (product.id) {
+    console.log('ratings', rating.ratings)
+    // let ratings = rating.ratings;
+    // let reviewCount = 0;
+    // let total = 0;
+    // for (let stars in ratings) {
+    //   reviewCount += ratings[stars];
+    //   total += (stars * ratings[stars]);
+    // }
+    // let avgRating = total / reviewCount;
+    // console.log('avg', avgRatings)
+  }
+
+  // }, [product])
 
   // useEffect(() => {
-  //   axios.get('/styles/' + currentId)
+  //   axios.get('/db/styles/' + currentId)
   //   .then((data) => {
   //     console.log(data)
   //   })
@@ -17,7 +31,6 @@ const Overview = ({ product }) => {
   //   .then((data) => {
   //     return (
   //       <div>
-
   //       </div>
   //     )
   //   })
@@ -28,6 +41,7 @@ const Overview = ({ product }) => {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p>{product.default_price}</p>
+      {/* <p>Read all {rating</p> */}
       {/* <img src={}/></img> */}
     </div>
   )
