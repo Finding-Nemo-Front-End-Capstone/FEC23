e3a214a55cc18f85236811dcf7a93e96adb43786
Nathanael <nathanaeltjen@hotmail.com>
2023-01-29 00:55:11 -0800

    Updated 12:54am Jan 29,2023

--- client/src/components/RatingsComp/PhotoEntry.jsx
+++ client/src/components/RatingsComp/PhotoEntry.jsx
@@ -3,10 +3,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function PhotoEntry(props) {
-  console.log(props.photo.url);
+  // console.log(props.photo.url);
   return (
     <div className="review-photo">
-      <img src={props.photo.url} />
+      <img class='eachReviewPhoto' src={props.photo.url} />
     </div>
   );
 }
