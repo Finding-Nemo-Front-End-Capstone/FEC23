2b07199216c8ec86571507531f7952cf81aa8bbb
Nathanael <nathanaeltjen@hotmail.com>
2023-01-29 00:23:04 -0800

    updated Jan 29, 2023

--- client/src/components/RatingsComp/PhotoEntry.jsx
+++ client/src/components/RatingsComp/PhotoEntry.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function PhotoEntry(props) {
   console.log(props.photo.url);
   return (
-    <div class='review-photo'>
-      <img src={props.photo.url}/>
+    <div className="review-photo">
+      <img src={props.photo.url} />
     </div>
   );
 }
