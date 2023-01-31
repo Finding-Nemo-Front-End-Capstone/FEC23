90c2542b7b915ea3733af81f79d4e9da64c90b3c
Britta Moore <brittacmoore@gmail.com>
2023-01-31 08:59:43 -0800

    stars and style selector are working

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,50 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Stars from './Stars.jsx';
 
 const Overview = ({ product, rating }) => {
-  console.log('current product', product)
 
-  if (product.id) {
-    console.log('ratings', rating.ratings)
-    // let ratings = rating.ratings;
-    // let reviewCount = 0;
-    // let total = 0;
-    // for (let stars in ratings) {
-    //   reviewCount += ratings[stars];
-    //   total += (stars * ratings[stars]);
-    // }
-    // let avgRating = total / reviewCount;
-    // console.log('avg', avgRatings)
+  const [styles, setStyles] = useState([]);
+  // const [activeIndex, setActiveIndex] = useState(0);
+  const [currStyle, setCurrStyle] = useState({});
+
+  useEffect(() => {
+    if (product.id) {
+      axios.get(`/db/styles/${product.id}`)
+      .then((data) => {
+        // console.log('styles',data.data.results)
+        setCurrStyle(data.data.results[0])
+        setStyles(data.data.results)
+      })
+      .catch((err) => { console.log('there was an error', err); })
+    }
+  }, [product])
+
+  // onClick for "Read all # reviews" -> scroll to reviews
+  // onClick for selected style - update images etc
+
+  if (styles.length > 0) {
+    return (
+      <div className="prod-content">
+        <div className="prod-info">
+          <h1 className="heading">{product.name}</h1>
+          {/* <p>{product.description}</p> */}
+          <p>${product.default_price}</p>
+          <Stars product={product} rating={rating}/>
+          <div className="style-wrapper">
+            <p>STYLE > {currStyle.name}</p>
+            <div className="styles">
+              {styles.map((style, i) => {
+                return <img className="thumbnails"
+                src={style.photos[0].thumbnail_url} key={style.style_id} index={i}
+                onClick={e => {setCurrStyle(styles[e.target.getAttribute('index')])}}/>
+              })}
+            </div>
+          </div>
+        </div>
+          <img className="main-photo" src={currStyle.photos[0].url}/>
+          <div className="gallery">
+              {currStyle.photos.map((photo, i) => {
+                return <img className="gallery-photos"
+                src={photo.thumbnail_url} key={photo.url} index={i}
+                />
+              })}
+            </div>
+      </div>
+    )
   }
 
-  // }, [product])
 
-  // useEffect(() => {
-  //   axios.get('/db/styles/' + currentId)
-  //   .then((data) => {
-  //     console.log(data)
-  //   })
-  //   .catch((err) => { console.log('there was an error', err); })
-  // }, [])
 
-  // const cards = relatedIds.map((singleId) => {
-  //   axios.get('/styles/' + singleId)
-  //   .then((data) => {
-  //     return (
-  //       <div>
-  //       </div>
-  //     )
-  //   })
-  // })
 
-  return (
-    <div>hi from Overview
-      <h1>{product.name}</h1>
-      <p>{product.description}</p>
-      <p>{product.default_price}</p>
-      {/* <p>Read all {rating</p> */}
-      {/* <img src={}/></img> */}
-    </div>
-  )
+
 };
 
 export default Overview;
+
+
+
+
+  // const avgRating = (ratingsObj) => {
+  //   let reviewCount = 0;
+  //   let total = 0;
+  //   if (ratingsObj) {
+  //     for (let stars in ratingsObj) {
+  //       reviewCount += Number(ratingsObj[stars]);
+  //       total += (stars * ratingsObj[stars]);
+  //     }
+  //   }
+  //   return (Math.round(total / reviewCount * 4) / 4).toFixed(2);
+  // }
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,35 +3,42 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import RelatedProducts from './components/RelatedProducts.jsx';
 import Questions from './components/Questions.jsx';
-// import Overview from './components/Overview.jsx';
+import Overview from './components/Overview.jsx';
 import Ratings from './components/Ratings.jsx';
+// import css from '../../client/dist/style.css';
 
 function App() {
 // const [productList, setProductList] = useState([]);
   const [product, setProduct] = useState({});
   const [rating, setRating] = useState({});
 
-  // useEffect(() => {
-  //   axios.get('/db/allProducts')
-  //     .then((data) => { setProduct(data.data[0]); })
-  //     .catch((err) => console.log(err));
-  // }, []);
+  useEffect(() => {
+    axios.get('/db/allProducts')
+      .then((data) => { setProduct(data.data[0]); })
+      .catch((err) => console.log(err));
+  }, []);
 
-  // useEffect(() => {
-  //   axios.get(`/db/meta/${product.id}`)
-  //     .then((data) => { console.log(data.data); })
-  //     .catch((err) => { console.log('meta did not work'); });
-  // }, [product]);
+  useEffect(() => {
+    if (product.id) {
+      axios.get(`/db/meta/${product.id}`)
+        .then((data) => { setRating(data.data); })
+        .catch((err) => { console.log('meta did not work'); });
+    }
+  }, [product]);
 
   return (
-    <div>
-      This is a placeholder being served
-      {/* <Overview product={product} rating={rating} /> */}
-      {/* <Ratings product={product} rating={rating} />
-      <Questions product={product} /> */}
-      <RelatedProducts product={product} setProduct={setProduct} />
-    </div>
+    <>
+      <nav className="nav-bar">Finding Nemo</nav>
+      <div>
+        <p className="site-announcement">SITE-WIDE ANNOUNCEMENT MESSAGE!</p>
+        <Overview product={product} rating={rating} />
+        {/* <Ratings product={product} rating={rating} />
+        <Questions product={product} /> */}
+        {/* <RelatedProducts product={product} setProduct={setProduct} /> */}
+      </div>
+    </>
   );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
