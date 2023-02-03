044be6a882598969d93597b9adbdd5dc701cd9a1
Sandy Chu <sandy.chu.40@gmail.com>
2023-02-03 12:12:45 -0800

    Update index.jsx
    Removing comments in index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -41,9 +41,9 @@ function App() {
   return (
     <div>
       <nav className="nav-bar">top bar</nav>
-      {/* <Overview product={product} rating={rating} />
+      <Overview product={product} rating={rating} />
       <Ratings product={product} rating={rating} setProduct={setProduct} />
-      <Questions product={product} /> */}
+      <Questions product={product} />
       <RelatedProducts id={product.id} product={prodInfo} rating={rating} currStyle={style}/>
     </div>
   );
