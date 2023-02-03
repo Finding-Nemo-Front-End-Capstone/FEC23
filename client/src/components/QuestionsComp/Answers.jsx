066b752efb3ec101b841cbf53c251b9c13bd17ef
George Khoury <gke@sent.com>
2023-02-03 02:45:33 -0800

    added helpful answer button

--- client/src/components/QuestionsComp/Answers.jsx
+++ client/src/components/QuestionsComp/Answers.jsx
@@ -10,7 +10,30 @@ function Answers({ question_id }) {
   const [displayed, setDisplayed] = useState([]);
   const [page, setPage] = useState(1);
   const [count, setCount] = useState(100);
-  function answerItem(answer) {
+  useEffect(() => {
+    if (question_id) {
+      axios({
+        url: `/db/answers/${question_id}?page=${page}&count=${count}`,
+        method: 'GET',
+      })
+      .then((response) => { setAllAnswers(response.data.results); });
+    }
+  }, []);
+  useEffect(() => {
+    const arr = [<span>A:</span>];
+    if (allAnswers[0] && allAnswers.length < numAnswers) {
+      for (let i = 0; i < allAnswers.length; i++) {
+        arr.push(displayAnswer(allAnswers[i]));
+      }
+      setDisplayed(arr);
+    } else if (allAnswers[0] && allAnswers.length > numAnswers) {
+      for (let i = 0; i < numAnswers; i++) {
+        arr.push(displayAnswer(allAnswers[i]));
+      }
+      setDisplayed(arr);
+    }
+  }, [allAnswers]);
+  function displayAnswer(answer) {
     return (
       <div>
         <li>
@@ -18,33 +41,15 @@ function Answers({ question_id }) {
         </li>
         <div>
           {`by ${answer.answerer_name} ${answer.date}`}
+          <button type="button" onClick={() => {helpfulAnswer(answer)}}>Helpful?</button>
         </div>
       </div>
     );
   }
-  useEffect(() => {
-    if (question_id) {
-      axios({
-        url: `/db/answers/${question_id}?page=${page}&count=${count}`,
-        method: 'GET',
-      })
-        .then((response) => { setAllAnswers(response.data.results); });
-    }
-  }, []);
-  useEffect(() => {
-    const arr = [<span>A:</span>];
-    if (allAnswers[0] && allAnswers.length < numAnswers) {
-      for (let i = 0; i < allAnswers.length; i++) {
-        arr.push(answerItem(allAnswers[i]));
-      }
-      setDisplayed(arr);
-    } else if (allAnswers[0] && allAnswers.length > numAnswers) {
-      for (let i = 0; i < numAnswers; i++) {
-        arr.push(answerItem(allAnswers[i]));
-      }
-      setDisplayed(arr);
-    }
-  }, [allAnswers]);
+  function helpfulAnswer(a) {
+    axios.put(`/db/helpfulanswer?answers_id=${a.answer_id}`)
+    .catch((err) => {console.log('err marking answer helpful', err)})
+  }
   return (
     <div>
       {displayed}
